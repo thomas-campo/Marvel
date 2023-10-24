de09bed1a1c9dae966ee86dff64fe013b703cf8f
Thomas campo <thomascampocood@gmail.com>
2023-10-24 18:55:54 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import handlebars  from "express-handlebars";
+import MongoStore from "connect-mongo";
+import mongoose from "mongoose";
+import passport from "passport";
+
+import ViewsRouter from "./router/views.router.js";
+
+// import initializePassport from "./config/passport.config.js";
+
+import __dirname from './utils.js';
+
+
+const app = express();
+const PORT = 8080;
+app.listen(PORT,()=>console.log(`escuchando en el puerto ${PORT}`));
+mongoose.connect("mongodb+srv://CoderUser:12345@cluster0.eb7exok.mongodb.net/?retryWrites=true&w=majority");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(`${__dirname}/public`));
+app.engine('handlebars',handlebars.engine());
+app.set('views',`${__dirname}/view`);
+app.set('view engine','handlebars');
+// app.use(passport.initialize());
+// initializePassport();
+
+app.use("/",ViewsRouter);
---
+++ src/public/js/home.js
@@ -0,0 +1,13 @@
+const API_URL = "https://gateway.marvel.com:443/v1/public/comics?format=comic&apikey=39011b8bf648e2690b47a9ba4ebd6bca";
+
+fetch(API_URL,{
+    method: "GET",
+    headers: {
+        'apikey' : "10bf2248c764fbd7c9ebdc0cbcc9412f80d9aa9e"
+    }
+})
+    .then(resp => resp.json())
+    .then(data =>{
+        console.log(data,"data")
+    })
+    .catch(error => console.log('Authorization failed : ' + error.message + error));
