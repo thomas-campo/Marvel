b3d415b8ed333d4ec08119aba8569c67e4aa7a21
Thomas campo <thomascampocood@gmail.com>
2023-10-24 20:55:36 -0300

    first commit

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,4 +1,4 @@
-const API_URL = "https://gateway.marvel.com:443/v1/public/comics?format=comic&apikey=39011b8bf648e2690b47a9ba4ebd6bca";
+const API_URL = "https://gateway.marvel.com:443/v1/public/comics?format=comic&apikey=39011b8bf648e2690b47a9ba4ebd6bca&hash=10bf2248c764fbd7c9ebdc0cbcc9412f80d9aa9e";
 
 fetch(API_URL,{
     method: "GET",
